38fee8cc93b15d09974ba716cee3dd5d82dfa72d
ssinha8752 <shubhamsinha8752@yahoo.com>
2023-12-03 03:12:46 +0530

    [Shubham][Pushing the Add functionality]

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
 
-const app=express();
-const port=3000;
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
+
+app.get("/addPost",(req,res)=>{
+    res.render("Add.ejs")
+})
+
+app.get("/updatePost",(req,res)=>{
+    res.send("Update Post")
+})
+
+app.get("/deletePost",(req,res)=>{
+    res.send("Delete Post")
+})
+
+app.post("/submit",(req,res)=>{
+    const latestContent=req.body.postContent;
+    res.render("index.ejs",{latestContent:latestContent});
+
+})
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
 
 app.listen(port,(req,res)=>{
     console.log("Listening at 3000");
